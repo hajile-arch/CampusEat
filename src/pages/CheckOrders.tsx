7362a0585c20e0948b023fe590bb4c58d7e9117e
rikashi-shifu <harryliow229@gmail.com>
2024-11-10 16:45:31 +0800

    chore: clean up

--- src/pages/CheckOrders.tsx
+++ src/pages/CheckOrders.tsx
@@ -3,29 +3,29 @@ import { readOrder } from "../api/order";
 import { OrderType } from "../types";
 
 const CheckOrders = () => {
-  const [myOrders, setMyOrders] = useState<OrderType[]>([]);
-  const [otherOrders, setOtherOrders] = useState<OrderType[]>([]);
+  // const [myOrders, setMyOrders] = useState<OrderType[]>([]);
+  // const [otherOrders, setOtherOrders] = useState<OrderType[]>([]);
 
-  useEffect(() => {
-    void (async () => {
-      readOrder("*").then(async (orders: OrderType[]) => {
-        const myOrderLists = orders?.filter((order) => {
-          return order.from_user_id.toString() === "B1234567";
-        });
-        setMyOrders(myOrderLists);
-        const otherOrderLists = orders?.filter((order) => {
-          return order.from_user_id.toString() !== "B1234567";
-        });
-        setOtherOrders(otherOrderLists);
-      });
-    })();
-  });
+  // useEffect(() => {
+  //   void (async () => {
+  //     readOrder("*").then(async (orders: OrderType[]) => {
+  //       const myOrderLists = orders?.filter((order) => {
+  //         return order.from_user_id.toString() === "B1234567";
+  //       });
+  //       setMyOrders(myOrderLists);
+  //       const otherOrderLists = orders?.filter((order) => {
+  //         return order.from_user_id.toString() !== "B1234567";
+  //       });
+  //       setOtherOrders(otherOrderLists);
+  //     });
+  //   })();
+  // });
   // const name1 = "Harry Liow";
   // const studentId1 = "B1234567";
 
   // const name2 = "Elijah Wong"
   // const studentId2 = "B2201651"
 
-  return null;
+  return <div>test</div>;
 };
 export default CheckOrders;
